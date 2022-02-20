4f837451b9242771918da9546b63f84d5f779527
DIMON <user357@rambler.ru>
2022-02-20 18:39:25 +0600

    final

--- app/src/main/java/com/Fragment/fragment/Fragment_B.java
+++ app/src/main/java/com/Fragment/fragment/Fragment_B.java
@@ -7,15 +7,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class Fragment_B extends Fragment implements MyInterface {
+   TextView textView2;
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.fragment_b,
                 container, false);
 
-        View textView2 = v.findViewById(R.id.textView2);
+        textView2 = v.findViewById(R.id.textView2);
 
 
 
@@ -24,7 +26,7 @@ public class Fragment_B extends Fragment implements MyInterface {
 
     @Override
     public void setResult(String s) {
-
+textView2.setText(s);
     }
 }
 
